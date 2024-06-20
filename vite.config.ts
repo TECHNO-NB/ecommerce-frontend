e57dcd1854c6b2ba5d7cc71f0af21ab4dea580de
NareshBhattarai4 <technologyandapplication@gmail.com>
2024-06-20 19:24:50 +0545

    new

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://ecommerce-backend-r13r.onrender.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api/v1'),
-      },
-    },
-  },
-  plugins: [react()],
+plugins: [react()],
 });
